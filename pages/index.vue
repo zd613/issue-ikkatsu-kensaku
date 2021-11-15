de6e4f1ea1a1c1aeb8bc8b61ac90c8023bfa85d2
ame <25921047+zd613@users.noreply.github.com>
2021-11-15 22:17:50 +0900

    ボタン押したときに検索するように変更

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
 
                 <div class="flex flex-row">
                     <input type="text" v-model="searchWord" />
-                    <button @click="">
+                    <button @click="searchIssues">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                     </svg>
@@ -137,6 +137,9 @@ const dep=ref("")
 const searchWord=ref("")
 
 
+
+
+const selectedPackagejson=ref<File|null>(null)
 async function handleFileChange(event: Event) {
     // TODO as any　ほかのやり方ないか調べる
     const files = (event.target as any).files as File[]
@@ -144,9 +147,20 @@ async function handleFileChange(event: Event) {
         return
     }
     const file = files[0]
+    selectedPackagejson.value=file;
+}
+
+
+async function searchIssues(){
+    const file=selectedPackagejson.value
+    if(!file){
+        alert("package.jsonを選択してください。")
+        return
+    }
 
     // json読み込み(package.json想定)
-    const text = await loadText(file)
+    // TODO: 型調べる
+    const text = await loadText(file as File)
     const json = JSON.parse(text)
 
     // package.jsonのdependenciesを取得する
@@ -175,6 +189,5 @@ async function handleFileChange(event: Event) {
         break
     }
 }
-
 
 </script>
