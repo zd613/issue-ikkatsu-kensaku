23d7e4868b911608347fed87fad3694e37361cfa
ame <25921047+zd613@users.noreply.github.com>
2021-11-15 22:12:00 +0900

    デザイン更新

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,42 @@
 
     <main>
       <article>
-        <h1 class="font-bold">hello world!</h1>
+          <div class="bg-blue-200 min-h-screen flex justify-center items-center">
+              <div class="flex flex-col">
+                <h1 class="text-5xl font-semibold text-gray-800 -mt-32">Issue一括検索</h1>
 
-        <input type="file" accept=".json" @change="handleFileChange" />
-
-        <div v-if="dep!==''">
-            {{dep}}
-        </div>
-        <div class="bg-gray-100">
-            <div v-for="(item, index) in issues" :key="index">
-                <a :href="item.html_url" target="_blank">
-                <div class="inline-block">
-                    {{ item.title }}
+                <div class="mt-12">
+                    <p>package.jsonに含まれているパッケージからissueを検索します。</p>
                 </div>
-                <div class="inline">({{ item.state }})</div>
-                </a>
-            </div>
-        </div>
+
+                <div class="flex flex-row">
+                    <input type="text" v-model="searchWord" />
+                    <button @click="">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                    </button>
+                </div>
+                <input type="file" accept=".json" @change="handleFileChange" />
+
+                <div v-if="dep!==''">
+                    {{dep}}
+                </div>
+                <div class="bg-gray-100">
+                    <div v-for="(item, index) in issues" :key="index">
+                        <a :href="item.html_url" target="_blank">
+                        <div class="inline-block">
+                            {{ item.title }}
+                        </div>
+                        <div class="inline">({{ item.state }})</div>
+                        </a>
+                    </div>
+                </div>
+              </div>
+
+          </div>
+
+
       </article>
     </main>
 
@@ -115,6 +134,9 @@ const issues=ref([] as IssueItem[])
 
 const dep=ref("")
 
+const searchWord=ref("")
+
+
 async function handleFileChange(event: Event) {
     // TODO as any　ほかのやり方ないか調べる
     const files = (event.target as any).files as File[]
@@ -141,7 +163,9 @@ async function handleFileChange(event: Event) {
         
         // issueを取得する
         let page = 1
-        let word = 'test' // 検索ワード
+        // let word = 'test' // 検索ワード
+
+        const word=searchWord.value;
         const issuesData = await fetchIssues(owner, repoName, word, page)
         console.log(issuesData)
 
