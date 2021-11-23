518b6106bcb177748d73b4f990e72e977e0297c0
ame <25921047+zd613@users.noreply.github.com>
2021-11-23 17:20:30 +0900

    検索iconにホバーした時のカーソルの形を変更

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border-4 border-green-500 p-2 inline-block">
     <input type="text" placeholder="検索ワード" v-model="searchWord" />
-    <IconSearch />
+    <IconSearch class="cursor-pointer" />
   </div>
 </template>
 
