5445b253ef48d0933db0e9e8fdd6dbcebb60c6cd
ame <25921047+zd613@users.noreply.github.com>
2021-11-23 15:56:51 +0900

    Create search.vue

---
+++ pages/search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>issue検索</h1>
+
+    <div>
+      <SearchBox />
+      <div>
+        <RepositoryList :repositories="repositoryList" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RepositoryInfo } from "~~/interfaces/RepositoryInfo";
+
+const repositoryList: RepositoryInfo[] = [
+  {
+    name: "hi",
+    url: "",
+  },
+  {
+    name: "hello",
+    url: "",
+  },
+];
+</script>
