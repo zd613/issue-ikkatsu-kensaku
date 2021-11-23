4816ed1265ebc334587dd8da6f0e49638488c315
ame <25921047+zd613@users.noreply.github.com>
2021-11-23 18:31:25 +0900

    使っていない部分を削除

--- components/FileSelector.vue
+++ components/FileSelector.vue
@@ -2,14 +2,10 @@
   <div>
     <span>package.jsonを選択する</span>
     <input type="file" accept=".json" @change="handleFileChange" />
-
-    {{ selectedPackagejson?.name ?? "not selected" }}
   </div>
 </template>
 
 <script setup lang="ts">
-import { PropType } from "vue";
-
 const props = defineComponent<{
   file: File | null;
 }>();
@@ -18,7 +14,6 @@ const emit = defineEmits<{
   (e: "update:file", file: File | null);
 }>();
 
-const selectedPackagejson = ref<File | null>(null);
 async function handleFileChange(event: Event) {
   // TODO as any　ほかのやり方ないか調べる
   const files = (event.target as any).files as File[];
@@ -27,7 +22,6 @@ async function handleFileChange(event: Event) {
     return;
   }
   const f = files[0];
-  selectedPackagejson.value = f;
   emit("update:file", f);
 }
 </script>
