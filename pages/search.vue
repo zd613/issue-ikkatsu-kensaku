d316ee4b3b6c5e86227f0711669601a7e1fd941c
ame <25921047+zd613@users.noreply.github.com>
2021-11-23 17:09:20 +0900

    レイアウト更新

--- pages/search.vue
+++ pages/search.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    <h1>issue検索</h1>
-
-    <div>
-      <SearchBox />
-      <FileSelector />
-      <div>
-        <RepositoryList :repositories="repositoryList" />
+  <div class="bg-blue-200 h-screen">
+    <div class="flex flex-row h-full">
+      <div class="w-64 p-3">
+        <h1 class="font-bold text-2xl p-1 text-gray-800 my-4">Issue一括検索</h1>
+        <div class="my-2">
+          <SearchBox />
+        </div>
+        <div class="my-6">
+          <FileSelector />
+        </div>
+        <div>
+          <RepositoryList :repositories="repositoryList" />
+        </div>
       </div>
-    </div>
 
-    <div>
-      <IssueList :issues="issueList" />
+      <div class="w-full h-full p-3">
+        <div class="flex flex-col items-center justify-center w-4/5 mx-auto">
+          <h2 class="font-bold text-3xl text-gray-800">検索結果</h2>
+
+          <div class="mt-12">
+            <IssueList :issues="issueList" />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
