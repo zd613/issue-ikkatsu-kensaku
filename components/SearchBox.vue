0b503c75af744e8621c74805e9026d1b1f124eac
ame <25921047+zd613@users.noreply.github.com>
2021-11-23 22:31:13 +0900

    検索欄でenterで検索できるように変更

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="border-4 border-green-500 p-2 inline-block">
-    <input type="text" placeholder="検索ワード" v-model="searchWord" />
-    <IconSearch class="cursor-pointer" @click="handleClick" />
+    <input
+      type="text"
+      placeholder="検索ワード"
+      v-model="searchWord"
+      @keydown.enter="search"
+    />
+    <IconSearch class="cursor-pointer" @click="search" />
   </div>
 </template>
 
@@ -12,7 +17,7 @@ const searchWord = ref("");
 const emits = defineEmits<{
   (e: "search", event: Event, searchWord: string);
 }>();
-const handleClick = (e) => {
+const search = (e) => {
   emits("search", e, searchWord.value);
 };
 </script>
