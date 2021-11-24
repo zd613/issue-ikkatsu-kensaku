0a2299fec42ab91ec145f19833eee268be59a34e
ame <25921047+zd613@users.noreply.github.com>
2021-11-25 00:10:27 +0900

    リポジトリホバー時色変更

--- components/RepositoryItem.vue
+++ components/RepositoryItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="border border-gray-500 p-1">
+    <div class="border border-gray-500 p-1 hover:bg-blue-300">
       <div>{{ repository.name }}</div>
     </div>
   </div>
