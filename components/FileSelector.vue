96aa83380d472af869ba4b7883a0ed978109b737
ame <25921047+zd613@users.noreply.github.com>
2021-11-27 17:36:18 +0900

    ファイルセレクターのデザイン変更

--- components/FileSelector.vue
+++ components/FileSelector.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
+    <!-- {{ file ?? "null" }} -->
     <label
       :for="fileSelectorId"
-      class="border-4 border-green-500 p-3 inline-block"
+      class="
+        border-2 border-gray-500
+        bg-gray-100
+        p-3
+        rounded-3xl
+        text-gray-800
+        flex
+        cursor-pointer
+        shadow-xl
+        hover:shadow-2xl
+      "
     >
-      <span class="inline-block">package.jsonを選択する</span>
-      <span v-if="selectedFile !== null">{{ selectedFile.name }}　選択中</span>
-      <input
-        type="file"
-        accept=".json"
-        @change="handleFileChange"
-        class="hidden"
-        :id="fileSelectorId"
-      />
+      <div>
+        <IconUpload class="w-16 h-16 text-gray-500" />
+      </div>
+      <div class="flex flex-col ml-4">
+        <span class="inline-block">package.jsonを選択する</span>
+        <div class="font-semibold text-2xl">
+          <p v-if="selectedFile === null" class="text-red-500">未選択</p>
+          <p v-else class="text-green-500">{{ selectedFile.name }}　選択中</p>
+        </div>
+
+        <input
+          type="file"
+          accept=".json"
+          @change="handleFileChange"
+          class="hidden"
+          :id="fileSelectorId"
+        />
+      </div>
     </label>
   </div>
 </template>
@@ -28,9 +48,14 @@ const emit = defineEmits<{
 
 const selectedFile = ref<File | null>(null);
 async function handleFileChange(event: Event) {
+  console.log("Handle file change");
   // TODO as any　ほかのやり方ないか調べる
   const files = (event.target as any).files as File[];
+  console.log(files);
+  console.log(files.length);
+  console.log(files.length === 0);
   if (files.length === 0) {
+    console.log("update file");
     emit("update:file", null);
     return;
   }
