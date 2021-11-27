2da94ff1b51b220dd5700cfdd119ff8ded5d7603
ame <25921047+zd613@users.noreply.github.com>
2021-11-27 18:04:29 +0900

    issueステータスの状態をアイコンで表示するように変更

--- components/IssueItem.vue
+++ components/IssueItem.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="border border-gray-800 p-2">
-    <a :href="issue.url" target="_blank">
-      <div>{{ issue.title }}</div>
-      <div>ステータス: {{ issue.state }}</div>
-      <div>createdAt: {{ formatCreatedAt }}</div>
-      <div>updatedAt: {{ formatUpdatedAt }}</div>
-      <!-- <div v-html="issue.body"></div> -->
+    <a :href="issue.url" target="_blank" class="flex flex-row">
+      <div class="flex-shrink-0 w-12 flex justify-center items-center">
+        <div v-if="issue.state === 'open'">
+          <IconIssueOpen class="text-green-500" />
+        </div>
+        <div v-else-if="issue.state === 'closed'">
+          <IconIssueClosed class="text-purple-500" />
+        </div>
+        <div v-else>{{ issue.state }}</div>
+      </div>
+      <div>
+        <div>{{ issue.title }}</div>
+        <div>ステータス: {{ issue.state }}</div>
+        <div>createdAt: {{ formatCreatedAt }}</div>
+        <div>updatedAt: {{ formatUpdatedAt }}</div>
+        <!-- <div v-html="issue.body"></div> -->
+      </div>
     </a>
   </div>
 </template>
@@ -13,6 +24,7 @@
 <script setup lang="ts">
 import { IssueInfo } from "~~/interfaces/issueInfo";
 import { format } from "date-fns";
+
 const props = defineProps<{
   issue: IssueInfo;
 }>();
