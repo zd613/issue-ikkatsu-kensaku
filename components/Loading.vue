9550964727a653f0cc29a5889a85efc2e34724bb
ame <25921047+zd613@users.noreply.github.com>
2021-11-28 07:23:06 +0900

    loadingコンポーネント作成

---
+++ components/Loading.vue
@@ -0,0 +1,20 @@
+<template>
+  <Loader :loading="loading" :size="size" :color="color" />
+</template>
+
+<script setup lang="ts">
+import Loader from "vue-spinner/src/BounceLoader.vue";
+
+withDefaults(
+  defineProps<{
+    loading?: boolean;
+    size?: string;
+    color?: string;
+  }>(),
+  {
+    loading: false,
+    size: "25px",
+    color: "#3B82F6",
+  }
+);
+</script>
